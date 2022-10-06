b2caa6978017a48178a97aa8bf50eb20fd87e17c
Group 12 HIT140 <112562291+Sudip-ghimire@users.noreply.github.com>
2022-10-06 15:23:20 +0930

    Update Group assignment 1.py
     import of tkinter, matplotlib,numpy,seaborn .....read the excel with coding done

--- Group assignment 1.py
+++ Group assignment 1.py
@@ -1,1 +1,21 @@
-Print hello
+#%matplotlib inline
+import tkinter
+import matplotlib
+import matplotlib.pyplot as plt
+mtplotlib.use('TKAgg')
+
+import numpy as np
+#%matplotlib inline
+import pandas as pd
+
+np.random.seed(10)
+
+import seaborn as sns
+
+
+#read the excel
+df=pd.read_csv('fair.csv')
+print(df)
+df.shape
+df.info()
+df.describe()
