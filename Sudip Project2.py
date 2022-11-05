778834cb87e97aea43a5dce798ec1c3311ec4743
Nabin-paudel <114154891+Nabin-paudel@users.noreply.github.com>
2022-11-05 12:43:48 +0930

    Update Sudip Project2.py

--- Sudip Project2.py
+++ Sudip Project2.py
@@ -5,3 +5,16 @@ import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
 import numpy as np
+import pandas as pd
+np.random.seed(10)
+import seaborn as sns
+
+#importing libraries
+from skleaarn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import matplotlip.pyplot as plt
+
+df = pd.read_csv('fair_new.csv')
+print(df)
+print(type(df))
